9eb107b8609b7171b427951c0b67a7310190ccc3
Opilane TTHK <krisdasel@gmail.com>
2024-05-03 12:05:27 +0300

    bleh 1

---
+++ hindeline too/hindeline too/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace hindeline_too
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inp = Convert.ToInt32(Console.ReadLine());
+            for ( int i = 0; i < inp; i++)
+            {
+                Console.WriteLine("Liptoni jäätee," + i + " purki, who wants it?");
+            }
+        }
+    }
+}
